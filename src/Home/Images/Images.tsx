ef0344b1e78c71467901d7d2bde673a6bee135f3
MehrabSp <mhrab832@gmail.com>
2023-09-06 00:42:37 +0330

    Working with Flatlist...

--- src/Home/Images/Images.tsx
+++ src/Home/Images/Images.tsx
@@ -1,93 +1,67 @@
-import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Image, FlatList } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { width } from "../../util/cc";
 import { MaterialIcons, Ionicons } from "@expo/vector-icons";
 
 const Images = () => {
-  return (
-    <ScrollView style={{ flex: 1, backgroundColor: "black" }}>
-      <StatusBar style="auto" />
-      <View>
-        <View
-          style={{
-            position: "absolute",
-            zIndex: 5,
-            bottom: 0,
-            flexDirection: "row",
-            margin: 20,
-          }}
-        >
-          <Ionicons name="ios-heart-outline" size={20} color="#fff" />
-          <Text style={styles.title}>10k</Text>
-        </View>
-        <Image
-          //   width={50}
-          source={{
-            uri: "https://d26oc3sg82pgk3.cloudfront.net/files/media/edit/image/49123/article_aligned%402x.jpg",
-          }}
-          style={{ height: 300, width: "100%", resizeMode: "cover" }}
-        />
-      </View>
 
-      <View style={{ flexDirection: "row" }}>
-        <View style={{ flexDirection: "column", justifyContent: "center" }}>
-          <Image
-            source={{
-              uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw81-kt1o_H5tbk3w7nsDIGaTOVZE7ApGUNA&usqp=CAU",
-            }}
-            style={{ width: "100%", height: width / 2 }}
-          />
-          <Image
-            source={{
-              uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS2q1SK-d1so1cNOabKmRj0a8y38_zI6uryA&usqp=CAU",
-            }}
-            style={{ width: width / 2, height: width / 2 }}
-          />
-        </View>
-        <View style={{ width, height: width }}>
-          <View
-            style={{
-              position: "absolute",
-              zIndex: 5,
-              bottom: 0,
-              flexDirection: "row",
-              margin: 20,
-            }}
-          >
-            <Ionicons name="ios-heart-outline" size={20} color="#fff" />
-            <Text style={styles.title}>10k</Text>
+  const DATA = [
+    {
+      id: "1",
+      image:
+        "https://e0.pxfuel.com/wallpapers/568/276/desktop-wallpaper-minimalist-dark.jpg",
+      style: styles.firstImage,
+    },
+    {
+      id: "2",
+      images: [
+        "https://e0.pxfuel.com/wallpapers/799/827/desktop-wallpaper-dark-female-black.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS2q1SK-d1so1cNOabKmRj0a8y38_zI6uryA&usqp=CAU",
+      ],
+      style: styles.twoImages,
+    },
+    {
+      id: "3",
+      images: [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw81-kt1o_H5tbk3w7nsDIGaTOVZE7ApGUNA&usqp=CAU",
+        "https://d26oc3sg82pgk3.cloudfront.net/files/media/edit/image/49123/article_aligned%402x.jpg",
+      ],
+      style: styles.twoImages,
+    },
+    {
+      id: "4",
+      image:
+        "https://d26oc3sg82pgk3.cloudfront.net/files/media/edit/image/49123/article_aligned%402x.jpg",
+      style: styles.lastImage,
+    },
+  ];
+
+  const renderItem = ({ item }) => {
+    if (item.images) {
+      return (
+        <View style={item.style}>
+          <Image source={{uri: item.images[0]}} style={styles.image} />
+          <View style={styles.twoImagesContainer}>
+            <Image source={{uri: item.images[1]}} style={styles.image} />
           </View>
-          <Image
-            source={{
-              uri: "https://e0.pxfuel.com/wallpapers/799/827/desktop-wallpaper-dark-female-black.jpg",
-            }}
-            style={{ width: "50%", height: width, resizeMode: "contain" }}
-          />
         </View>
-      </View>
+      );
+    }
+    return (
+      <Image source={{uri: item.image}} style={[styles.image, item.style]} />
+    );
+  };
 
-      <View>
-        <View
-          style={{
-            position: "absolute",
-            zIndex: 5,
-            bottom: 0,
-            flexDirection: "row",
-            margin: 20,
-          }}
-        >
-          <Ionicons name="ios-heart-outline" size={20} color="#fff" />
-          <Text style={styles.title}>10k</Text>
-        </View>
-        <Image
-          source={{
-            uri: "https://d26oc3sg82pgk3.cloudfront.net/files/media/edit/image/49123/article_aligned%402x.jpg",
-          }}
-          style={{ height: 300, width: "100%", resizeMode: "cover" }}
-        />
-      </View>
-    </ScrollView>
+  return (
+    <View style={{ flex: 1, backgroundColor: "yellow" }}>
+      <StatusBar style="auto" />
+      <FlatList
+      data={DATA}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+    />
+    </View>
   );
 };
 
@@ -96,5 +70,24 @@ export default Images;
 const styles = StyleSheet.create({
   title: {
     color: "white",
+  },
+  image: {
+    flex: 1,
+    height: "100%",
+    resizeMode: "cover",
+  },
+  firstImage: {
+    height: 200,
+  },
+  twoImages: {
+    height: 200,
+    flexDirection: "row",
+  },
+  twoImagesContainer: {
+    flex: 1,
+    // marginLeft: 5,
+  },
+  lastImage: {
+    height: 250,
   },
 });
