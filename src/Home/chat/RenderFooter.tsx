82ed29a4ffb6b7738d3fb1c80c183f5e5a81a914
MehrabSp <mhrab832@gmail.com>
2023-09-16 23:52:26 +0330

    add footer and fix for screens

---
+++ src/Home/chat/RenderFooter.tsx
@@ -0,0 +1,36 @@
+import { View, Text, Image } from "react-native";
+
+export const RenderFooter = ({ item, fixPos }) => {
+  console.log(fixPos.current.numFooter, "num footer");
+  if (fixPos.current.numFooter > 3){
+    fixPos.current.numFooterPlus = fixPos.current.numFooterPlus + 1;
+    return (
+      <View
+        style={{
+          backgroundColor: '#e1e0e1',
+          height: 40,
+          width: 40,
+          borderRadius: 10,
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Text style={{fontWeight: 'bold', fontSize: 15, color: '#636181'}}>{fixPos.current.numFooterPlus}+</Text>
+      </View>
+    );
+  }
+  fixPos.current.numFooter = fixPos.current.numFooter + 1;
+  return (
+    <View
+      style={{
+        // flexDirection: "row", //this
+        marginRight: 10,
+      }}
+    >
+      <Image
+        source={{ uri: item.profile_image }}
+        style={{ height: 40, width: 40, borderRadius: 10 }}
+      />
+    </View>
+  );
+};
--- src/Home/chat/index.tsx
+++ src/Home/chat/index.tsx
@@ -5,49 +5,86 @@ import {
   FlatList,
   TextInput,
 } from "react-native";
-import { useState, useRef, useCallback } from "react";
+import { useState, useRef } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Header } from "./Header";
 import { Feather } from "@expo/vector-icons";
 import { chat_data } from "./data";
 import { Render } from "./Render";
+import { RenderFooter } from "./RenderFooter";
 
 export const Chat = () => {
-  const [me, setMe] = useState("Tim Russel");
+  const [me, setMe] = useState("Tim Russel"); //Re-render all
   const fixPos = useRef({
     lastName: null,
     Ids: null,
+    numFooter: 0,
+    numFooterPlus: 1,
   });
   const { height, width } = useWindowDimensions();
 
   function handleChangheMe(name: string) {
-    if (name != me)
-      setMe(name)
+    if (name != me) setMe(name);
   }
 
   const Columns: boolean = width > height;
   let styleScrollView = {};
   let styleHeader = {};
+  let styleFooter = {};
   if (Columns) {
     // چرخش فعال یا تبلت
+    fixPos.current.numFooter = 0; // هندل کردن فوتر موقع چرخش صفحه
+    fixPos.current.numFooterPlus = 1; // هندل کردن فوتر موقع چرخش صفحه
     styleScrollView = {
       minHeight: 100,
-      maxHeight: 200,
+      maxHeight: "15%",
       marginTop: 10,
+      paddingHorizontal: 40,
     };
     styleHeader = {
       marginTop: 5,
     };
+    styleFooter = {
+      position: "absolute",
+      top: 0,
+      borderRadius: 10,
+      padding: 10,
+      right: '10%',
+    };
   } else {
     // حالت عادی موبایل
+    fixPos.current.numFooter = 0; // هندل کردن فوتر موقع چرخش صفحه
+    fixPos.current.numFooterPlus = 1; // هندل کردن فوتر موقع چرخش صفحه
     styleScrollView = {
       minHeight: 370,
-      maxHeight: 370,
+      maxHeight: "37%",
       marginTop: 25,
+      paddingHorizontal: 18,
     };
     styleHeader = {
       marginTop: 25,
     };
+    styleFooter = {
+      position: "absolute",
+      top: height - 60 * 2,
+      alignItems: "flex-end",
+      borderRadius: 10,
+      alignSelf: "flex-end",
+      padding: 10,
+      marginTop: 20,
+      right: 10,
+    };
+  }
+
+  const seen = {};
+  const newFooterItems = []; // New items for footer images
+
+  for (let i = 0; i < chat_data.length; i++) {
+    const name = chat_data[i].name;
+    if (!seen[name]) {
+      seen[name] = true;
+      newFooterItems.push(chat_data[i]);
+    }
   }
 
   return (
@@ -63,7 +100,6 @@ export const Chat = () => {
                 borderTopWidth: 1,
                 borderColor: "#f5f5f5",
                 marginHorizontal: 10,
-                paddingHorizontal: 18,
                 // paddingVertical: 30,
               },
               styleScrollView,
@@ -111,6 +147,28 @@ export const Chat = () => {
             >
               <Feather name="send" size={20} color="white" />
             </View>
+          </View>
+
+          <View
+            style={[
+              {
+                backgroundColor: "whitesmoke",
+                shadowColor: "#000",
+                shadowOffset: { width: 4, height: 4 },
+                shadowOpacity: 0.7,
+                shadowRadius: 15,
+              },
+              styleFooter,
+            ]}
+          >
+            <FlatList
+              data={newFooterItems}
+              renderItem={({ item }) => RenderFooter({ item, fixPos })}
+              key={"IMG-Footer"}
+              horizontal
+              scrollEnabled={false}
+              keyExtractor={(_, index) => "$" + index}
+            />
           </View>
 
           {/* Footer  */}
